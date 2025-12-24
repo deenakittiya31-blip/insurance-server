da62e59df218f37f24e7fab2717c11e1db9649a2
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2025-12-24 13:12:33 +0700

    delete multer file

--- routes/insurancCompany.js
+++ routes/insurancCompany.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const { authCheck } = require('../middleware/authCheck')
 const { create, list, update, remove, read } = require('../controllers/insurancCompany')
-const upload = require('../middleware/upload')
 const router = express.Router()
 
-router.post('/create-company', authCheck, upload.single('logo_url'), create)
+router.post('/create-company', authCheck, create)
 router.get('/list-company', list)
 router.get('/list-company-select', list)
 router.get('/read-company/:id', authCheck, read)
