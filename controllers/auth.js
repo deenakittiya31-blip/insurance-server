fb3bacf770ec409d4c83d8250cd84cbd0082ab2d
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2025-12-25 13:59:13 +0700

    array value

--- controllers/auth.js
+++ controllers/auth.js
@@ -31,7 +31,7 @@ exports.register = async(req, res)=>{
 
 exports.currentUser = async(req, res) => {
     try {
-        const result = await db.query('SELECT name, email, image, role FROM users WHERE email = $1', req.user.email)
+        const result = await db.query('SELECT name, email, image, role FROM users WHERE email = $1', [req.user.email])
 
         res.json({ user: result.rows[0]})
     } catch (err) {
