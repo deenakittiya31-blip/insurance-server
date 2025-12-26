ce2e50b774aa77ff057b290e85835c86d0c63b4a
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2025-12-26 15:20:37 +0700

    route read cartype

--- routes/carType.js
+++ routes/carType.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const { authCheck, roleCheck } = require('../middleware/authCheck')
-const { create, list, update, remove, listSelect } = require('../controllers/carType')
+const { create, list, update, remove, listSelect, read } = require('../controllers/carType')
 const router = express.Router()
 
 router.post('/create-cartype', authCheck, roleCheck(['admin']), create)
 router.get('/list-cartype/page', list)
 router.get('/list-cartype-select', listSelect)
+router.get('/read-cartype/:id', authCheck, roleCheck(['admin']),read)
 router.put('/update-cartype/:id', authCheck, roleCheck(['admin']), update)
 router.delete('/delete-cartype/:id', authCheck, roleCheck(['admin']), remove)
 
