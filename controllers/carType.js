b26eea5e023b79268e23942b4e4b58600cc194fa
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2025-12-26 14:46:31 +0700

    edit cartype

--- controllers/carType.js
+++ controllers/carType.js
@@ -2,9 +2,9 @@ const db = require('../config/database')
 
 exports.create = async(req, res) => {
     try {
-        const { type } = req.body
+        const { type, code, car_usage_id } = req.body
 
-        await db.query('INSERT INTO car_type(type) VALUES($1)', [type])
+        await db.query('INSERT INTO car_type(type, code, car_usage_id) VALUES($1, $2, $3)', [type, code, car_usage_id])
 
         res.json({ msg: 'เพิ่มประเภทรถสำเร็จ' })
     } catch (err) {
@@ -20,7 +20,7 @@ exports.list = async(req, res) => {
         const offset = (page - 1) * per_page
 
     try {
-        const result = await db.query('SELECT id, type FROM car_type ORDER BY id ASC LIMIT $1 OFFSET $2', [per_page, offset])
+        const result = await db.query('SELECT ct.id, ct.type, ct.code, cu.usage_name FROM car_type as ct JOIN car_usage as cu ON ct.car_usage_id = cu.id ORDER BY id ASC LIMIT $1 OFFSET $2', [per_page, offset])
 
         const countResult = await db.query('SELECT COUNT(*)::int as total FROM car_type')
 
@@ -42,12 +42,34 @@ exports.listSelect = async(req, res) => {
     }
 }
 
-exports.update = async(req, res) => {
+exports.read = async(req,res)=>{
     try {
-        const { type } = req.body;
-        const { id } = req.params;
+        const { id } = req.params
+        const result = await db.query('SELECT type, code, car_usage_id FROM car_type WHERE id = $1', [id])
 
-        await db.query('UPDATE car_type SET type = $1 WHERE id = $2', [type, id])
+
+        res.json({data: result.rows[0]})
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({message: 'server error'})
+    }
+}
+
+exports.update = async(req, res) => {
+    const { type, code, car_usage_id } = req.body;
+    const { id } = req.params;
+    try {
+        const result = await db.query('SELECT * FROM car_type WHERE id = $1', [id])
+
+        const old = result.rows[0]
+
+        await db.query('UPDATE car_type SET type = $1, code = $2, car_usage_id = $3 WHERE id = $4', 
+            [
+                type                ?? old.type,
+                code                ?? old.code, 
+                car_usage_id        !== undefined ? Number(car_type_id) : old.car_usage_id,
+                id
+            ])
 
         res.json({msg: 'แก้ไขประเภทรถสำเร็จ'}) 
     } catch (err) {
