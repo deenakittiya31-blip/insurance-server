74e3ddc085488e6529397024e3cba702dc7f6888
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2025-12-26 09:32:58 +0700

    add list

--- routes/insurancPackage.js
+++ routes/insurancPackage.js
@@ -4,7 +4,7 @@ const { create, list, update, remove, listSelect, read } = require('../controlle
 const router = express.Router()
 
 router.post('/create-package', authCheck, roleCheck(['admin']), create)
-router.get('/list-package', list)
+router.get('/list-package/page', list)
 router.get('/list-package-select', listSelect)
 router.get('/read-package/:id', authCheck, roleCheck(['admin']), read)
 router.put('/update-package/:id', authCheck, roleCheck(['admin']), update)
