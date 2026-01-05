d42ba80b7b740998e9b94087e2acf8b87567e663
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-05 15:39:14 +0700

    multifile

--- controllers/aksonocr.js
+++ controllers/aksonocr.js
@@ -5,31 +5,41 @@ const api = 'https://backend.aksonocr.com/api/v1/key-extract-url'
 
 exports.akson = async(req, res) => {
     try {
-        const {base64} = req.body;
-        console.log(base64)
+        const {images} = req.body;
+        console.log(images)
 
-        if(!base64){
-             return res.status(400).json({ msg: 'image ต้องเป็น base64 string' })
+        if (!Array.isArray(images) || images.length === 0) {
+            return res.status(400).json({
+            success: false,
+            message: 'images ต้องเป็น array ของ base64'
+            })
         }
 
-        const payload = {
-            base64Image: base64,
-            customFields:  field,
-            additionalInstructions: "Extract numbers without commas or currency symbols. Dates in DD/MM/YYYY format."
+        const results = []
+
+        for (let i = 0; i < images.length; i++) {
+            const payload = {
+                base64Image: base64,
+                customFields:  field,
+                additionalInstructions: "Extract numbers without commas or currency symbols. Dates in DD/MM/YYYY format."
+            }
+
+            const response = await axios.post(api, payload, {
+                headers: {
+                    "X-API-Key": process.env.AKSON_KEY,
+                    "Content-Type": "application/json"
+                },
+                timeout: 60000
+            })
+
+            results.push({
+                index: i,
+                ocr: response.data.data
+            })
         }
 
-        const response = await axios.post(api, payload, {
-            headers: {
-                "X-API-Key": process.env.AKSON_KEY,
-                "Content-Type": "application/json"
-            },
-            timeout: 60000
-        })
-
-        return res.json({
-            success: true,
-            data: response.data.data
-        })   
+           return res.json({data: results})  
+            
     } catch (err) {
         console.error(err.response?.data || err.message)
         return res.status(500).json({
