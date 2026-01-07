b2c2c482ec1703c7debba7b55b7e17e7dc93fa60
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-07 14:04:28 +0700

    update route

--- routes/insurancCompany.js
+++ routes/insurancCompany.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const { authCheck, roleCheck } = require('../middleware/authCheck')
-const { create, list, update, remove, read } = require('../controllers/insurancCompany')
+const { create, list, update, remove, read, listSelect } = require('../controllers/insurancCompany')
 const router = express.Router()
 
 router.post('/create-company', authCheck, roleCheck(['admin']), create)
 router.get('/list-company/page', list)
-router.get('/list-company-select', list)
+router.get('/list-company-select', listSelect)
 router.get('/read-company/:id', authCheck, roleCheck(['admin']), read)
 router.put('/update-company/:id', authCheck, roleCheck(['admin']), update)
 router.delete('/delete-company/:id', authCheck, roleCheck(['admin']), remove)
