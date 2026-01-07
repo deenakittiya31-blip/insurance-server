534409c3bc3d7679367b1978d23fe477bf61207c
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-07 15:16:44 +0700

    route error

--- routes/compare.js
+++ routes/compare.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const { authCheck, roleCheck } = require('../middleware/authCheck');
-const { getQuotationDetail, createCompare } = require('../controllers/compare');
+const { createCompare } = require('../controllers/compare');
 const router = express.Router();
 
 router.post('/create-compare', authCheck, roleCheck(['admin']), createCompare)
-router.get('/detail-capare/:id', authCheck, roleCheck(['admin']), getQuotationDetail)
 
 module.exports = router
