fc864088335aa6484240f5779be1b84d5c3d63c3
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-08 10:47:32 +0700

    sql

--- controllers/compare.js
+++ controllers/compare.js
@@ -57,7 +57,7 @@ exports.getDetailCompare = async(req, res) => {
             return res.status(404).json({ msg: 'ไม่พบข้อมูล' })
         }
 
-        const result = await db.query('select qc.q_id, cb.name as car_brand, cm.name as car_model, cu.usage_name as usage, cy.year_be || '/' || cy.year_ad as year from quotation_compare as qc join car_brand as cb on qc.car_brand_id = cb.id join car_model as cm on qc.car_model_id = cm.id join car_usage as cu on qc.car_usage_id = cu.id join car_year as cy on qc.car_year_id = cy.id where qc.q_id::text = $1',[id])
+        const result = await db.query('select qc.q_id, cb.name as car_brand, cm.name as car_model, cy.year_be, cy.year_ad from quotation_compare as qc join car_brand as cb on qc.car_brand_id = cb.id join car_model as cm on qc.car_model_id = cm.id join car_usage as cu on qc.car_usage_id = cu.id join car_year as cy on qc.car_year_id = cy.id where qc.q_id::text = $1',[id])
 
         res.json({ data: result.rows[0]})
     } catch (err) {
