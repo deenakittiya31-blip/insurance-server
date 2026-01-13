cdd022c1aab1d7574e1e96ceb0da339a17ca8604
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-13 11:14:30 +0700

    row heigth

--- utils/generatePDF.js
+++ utils/generatePDF.js
@@ -221,7 +221,7 @@ async function drawTableContent(doc, insurances) {
                 tableX + 5,
                 tableY,
                 col1 - 10,
-                rowHeight,
+                rowHeight + 5,
                 { align: 'left' }
             );
 
