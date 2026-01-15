eec9b24c1669bbb30543c40aca170f3609761ee9
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-15 11:06:36 +0700

    add car_usage column in compulsory

--- controllers/compulsoryInsur.js
+++ controllers/compulsoryInsur.js
@@ -30,7 +30,7 @@ exports.list = async(req, res) => {
     const offset = (page - 1) * per_page
 
     try {
-        const result = await db.query('SELECT ci.id, ct.type, ci.code, net_price, vat, stamp, total, detail FROM compulsory_insurance as ci INNER JOIN car_type as ct ON ci.car_type_id = ct.id ORDER BY ci.id ASC LIMIT $1 OFFSET $2', [per_page, offset])
+        const result = await db.query('SELECT ci.id, cu.usage_name as usage, ct.type, ci.code, net_price, vat, stamp, total FROM compulsory_insurance as ci INNER JOIN car_type as ct ON ci.car_type_id = ct.id left join car_usage as cu on ct.car_usage_id = cu.idORDER BY ci.id ASC LIMIT $1 OFFSET $2', [per_page, offset])
 
         const countResult = await db.query('SELECT COUNT(*)::int as total FROM compulsory_insurance')
 
