56de0baa44945aad607d86c2af63b4dfea1a1fa8
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-16 16:19:49 +0700

    read field model

--- routes/customodel.js
+++ routes/customodel.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { authCheck, roleCheck } = require('../middleware/authCheck');
-const { listModel, removeModel, updateModelFields,  createModelAndFields, readModel } = require('../controllers/custommodel');
+const { listModel, removeModel, updateModelFields,  createModelAndFields, readModel, readFieldsModel } = require('../controllers/custommodel');
 const router = express.Router();
 
 router.post('/create-modelandfields', authCheck, roleCheck(['admin']), createModelAndFields)
 router.get('/list-model/page', authCheck, roleCheck(['admin']), listModel)
 router.get('/read-model/:id', authCheck, roleCheck(['admin']), readModel)
+router.get('/read-fieldsmodel/:id', authCheck, roleCheck(['admin']), readFieldsModel)
 router.delete('/delete-model/:id', authCheck, roleCheck(['admin']), removeModel)
 router.patch('/update-model/:id', authCheck, roleCheck(['admin']), updateModelFields)
 
