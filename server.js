5bc16f3008c44d916c1543017b583f86226c8f5e
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-18 15:58:56 +0700

    core access

--- server.js
+++ server.js
@@ -6,6 +6,19 @@ const { readdirSync } = require('fs')
 
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://insuurance-client.vercel.app')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.header('Access-Control-Allow-Credentials', 'true')
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+
+  next()
+})
+
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json({ limit: '10mb'}))
