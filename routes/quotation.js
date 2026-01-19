46038bd5425a5bf50b0f8cde80a1abe57ac46b3c
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-19 09:22:12 +0700

    blocked by cors route error

--- routes/quotation.js
+++ routes/quotation.js
@@ -4,8 +4,7 @@ const { removeQuotation, createQuotationFields, createFields, readQuotationField
 const router = express.Router();
 
 router.post('/create-quotation/fields', authCheck, roleCheck(['admin']), createQuotationFields)
-router.get('/read-quotation/fields/:id', authCheck, roleCheck(['admin']), readQuotationFields)
-router.put('/update-quotation/fields', authCheck, roleCheck(['admin']), updateQuotationField)
+
 //สำหรับสร้าง quotation และ fields แบบ key-in
 router.post('/create-quotationandfields', authCheck, roleCheck(['admin']), createFields)
 router.delete('/delete-quotation/:id', authCheck, roleCheck(['admin']), removeQuotation)
