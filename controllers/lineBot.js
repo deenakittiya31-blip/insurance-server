6ee50f21e6cf33f36596f8335b438b059961b83d
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-20 07:58:39 +0700

    reply text line chatbot

--- controllers/lineBot.js
+++ controllers/lineBot.js
@@ -1,2 +1,35 @@
-const db = require('../config/database')
+const request = require("request-");
+const axios = require('axios');
+
+const LINE_MESSAGING_API = "https://api.line.me/v2/bot/message/reply";
+const LINE_HEADER = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`
+};
+
+exports.lineBotReply = async(req, res) => {
+    try {
+        if (req.method === "POST"){
+            await reply(req.body);
+            return res.status(200).send('OK')
+        } else {
+            return res.status(200).send(`Done`);
+        }
+    } catch (err) {
+        console.error(err.response?.data || err.message)
+        return res.status(500).send('Error')
+    }
+}
+
+const reply = async(bodyResponse) => {
+  return axios.post(LINE_MESSAGING_API, 
+        { 
+            replyToken: bodyResponse.events[0].replyToken, 
+            messages: [{ type: text, text: JSON.stringify(bodyResponse) }] 
+        }, 
+        { 
+            headers: LINE_HEADER 
+        }
+    )
+};
 
--- routes/lineBot.js
+++ routes/lineBot.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { roleCheck, authCheck } = require('../middleware/authCheck');
+const { lineBotReply } = require('../controllers/lineBot');
 const router = express.Router();
 
-router.post('/webhook', authCheck, roleCheck(['admin']), )
+router.post('/webhook', authCheck, roleCheck(['admin']), lineBotReply)
 
 module.exports = router
