97179651efd144243c7ea88b3da5c1717d6b2361
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-22 20:29:56 +0700

    route compare list

--- routes/compare.js
+++ routes/compare.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { authCheck, roleCheck } = require('../middleware/authCheck');
-const { createCompare, getDetailCompare, comparePDF, compareJPG } = require('../controllers/compare');
+const { createCompare, getDetailCompare, comparePDF, compareJPG, listCompare } = require('../controllers/compare');
 const router = express.Router();
 
+router.get('/list-compare/page', authCheck, roleCheck(['admin']), listCompare)
 router.post('/create-compare', authCheck, roleCheck(['admin']), createCompare)
 router.get('/detail-compare/:id', authCheck, roleCheck(['admin']), getDetailCompare)
 router.get('/pdf-compare/:id', authCheck, roleCheck(['admin']), comparePDF)
