c7d0ef0d2ef67d3d9cd53d5a4231d13ccf711f54
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-01-23 12:22:46 +0700

    remove function

--- routes/image.js
+++ routes/image.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const { authCheck, roleCheck } = require('../middleware/authCheck')
-const { createImages, removeImages, uploadDocument } = require('../controllers/image')
+const { createImages, removeImages } = require('../controllers/image')
 const router = express.Router()
 
 router.post('/images', authCheck, roleCheck(['admin']), createImages)
-// router.post('/document', authCheck, roleCheck(['admin']), uploadDocument)
 router.post('/removeImages', authCheck, roleCheck(['admin']), removeImages)
 
 module.exports = router
