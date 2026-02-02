920efd471f6adccba6788fce324446f03ced9436
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-02-02 10:35:34 +0700

    is_active function

--- controllers/carType.js
+++ controllers/carType.js
@@ -13,6 +13,21 @@ exports.create = async(req, res) => {
     }
 }
 
+exports.is_active = async(req, res) => {
+    try {
+            const {is_active} = req.body
+            const {id} = req.params
+
+            await db.query('UPDATE car_type SET is_active = $1 WHERE id = $2', 
+            [is_active, id])
+
+        res.json({msg: 'อัปเดตประเภทรถยนต์สำเร็จ'})  
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({message: 'server errer'})
+    }
+}
+
 exports.list = async(req, res) => {
     const page = Number(req.query.page) || 1;
     const per_page = Number(req.query.per_page) || 5;
--- routes/carType.js
+++ routes/carType.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { authCheck, roleCheck } = require('../middleware/authCheck')
-const { create, list, update, remove, listSelect, read } = require('../controllers/carType')
+const { create, list, update, remove, listSelect, read, is_active } = require('../controllers/carType')
 const router = express.Router()
 
 router.post('/create-cartype', authCheck, roleCheck(['admin']), create)
@@ -8,6 +8,7 @@ router.get('/list-cartype/page', list)
 router.get('/list-cartype-select', listSelect)
 router.get('/read-cartype/:id', authCheck, roleCheck(['admin']),read)
 router.put('/update-cartype/:id', authCheck, roleCheck(['admin']), update)
+router.put('/status-cartype/:id', authCheck, roleCheck(['admin']), is_active)
 router.delete('/delete-cartype/:id', authCheck, roleCheck(['admin']), remove)
 
 module.exports = router
