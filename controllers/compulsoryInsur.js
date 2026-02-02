eab9a4d757c17a4c76173c737e29f76c3da7212a
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-02-02 14:50:15 +0700

    comma

--- controllers/compulsoryInsur.js
+++ controllers/compulsoryInsur.js
@@ -4,7 +4,7 @@ exports.create = async(req, res) => {
     try {
         const { car_type, car_usage_type_id, code_sub, detail, net_price, vat, stamp, total } = req.body
 
-        await db.query('INSERT INTO compulsory_insurance (car_type, car_usage_type_id, code_sub, detail net_price, vat, stamp, total) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
+        await db.query('INSERT INTO compulsory_insurance (car_type, car_usage_type_id, code_sub, detail, net_price, vat, stamp, total) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', 
                             [
                                 car_type, 
                                 Number(car_usage_type_id), 
