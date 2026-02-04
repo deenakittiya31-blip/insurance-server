5b6ae3843572c6564d12583ee29eb8177f40b91e
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-02-04 17:13:52 +0700

    delete group

--- routes/group_member.js
+++ routes/group_member.js
@@ -6,6 +6,6 @@ const router = express.Router()
 router.post('/create-groupmember', authCheck, roleCheck(['admin', 'staff']), create)
 router.get('/list-groupmember', authCheck, roleCheck(['admin', 'staff']), list)
 router.put('/update-groupmember/:id', authCheck, roleCheck(['admin', 'staff']), update)
-router.delete('/delete-groupmember', authCheck, roleCheck(['admin', 'staff']), remove)
+router.delete('/delete-groupmember/:id', authCheck, roleCheck(['admin', 'staff']), remove)
 
 module.exports = router
