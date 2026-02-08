1d1f0b3f61370f862016aaa5e2bdbbe3354ec235
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-02-08 23:05:03 +0700

    authcheck

--- routes/messageLine.js
+++ routes/messageLine.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { sendMessageLine } = require('../controllers/messageLine')
+const { authCheck, roleCheck } = require('../middleware/authCheck');
 const router = express.Router()
 
 router.post('/send-message', authCheck, roleCheck(['admin', 'staff']), sendMessageLine)
