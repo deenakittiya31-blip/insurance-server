29fb3f8e253e69be3dfcc98b2baef8d745d204bd
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-02-09 14:04:19 +0700

    tag

--- controllers/tag.js
+++ controllers/tag.js
@@ -36,7 +36,7 @@ exports.list = async(req, res) => {
 
 exports.listSelect = async(req, res) => {
     try {
-        const result = await db.query('SELECT id, tag_named FROM tag WHERE is_active = true ORDER BY id DESC')
+        const result = await db.query('SELECT id, tag_name FROM tag WHERE is_active = true ORDER BY id DESC')
          if (result.rows.length === 0) {
             return res.json({ data: [] })
         }
@@ -47,19 +47,6 @@ exports.listSelect = async(req, res) => {
         res.status(500).json({msg: 'Server errer'})
     }
 }
-
-// exports.read = async(req,res)=>{
-//     try {
-//         const { id } = req.params
-//         const result = await db.query('SELECT id, tag_name FROM tag WHERE id = $1', [id])
-
-
-//         res.json({data: result.rows[0]})
-//     } catch (err) {
-//         console.log(err)
-//         res.status(500).json({message: 'server error'})
-//     }
-// }
 
 exports.update = async(req, res) => {
     const { tag_name } = req.body;
