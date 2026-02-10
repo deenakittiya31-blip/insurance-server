220e3a5f53a511b5fefebe8c5b60cdf87544fdb4
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-02-10 07:59:53 +0700

    tags

--- controllers/messageLine.js
+++ controllers/messageLine.js
@@ -3,17 +3,39 @@ const db = require('../config/database');
 
 exports.sendMessageLine = async(req, res) => {
     try {
-        const { text, image, members } = req.body
+        const { text, image, members, tags } = req.body
 
-        if (!members || !members.length) {
-            return res.status(400).json({ message: 'กรุณาเลือกสมาชิกก่อน' });
+        if (members.length === 0 && tags.length === 0) {
+            return res.status(400).json({ message: 'กรุณาเลือกสมาชิกหรือแท็กอย่างน้อย 1 รายการ' });
         }
 
-        const result = await db.query(
-            `SELECT user_id FROM member 
-             WHERE is_active = true AND user_id = ANY($1)`,
-            [members]
-        )
+        let query = `
+            SELECT DISTINCT m.user_id
+            FROM member m
+            LEFT JOIN tag_member tm ON m.id = tm.member_id
+            WHERE m.is_active = true
+        `
+
+        const conditions = []
+        const values = []
+
+        // ส่งตาม member ที่เลือก
+        if (members.length > 0) {
+            values.push(members)
+            conditions.push(`m.user_id = ANY($${values.length})`)
+        }
+
+        // ส่งตาม tag
+        if (tags.length > 0) {
+            values.push(tags)
+            conditions.push(`tm.tag_id = ANY($${values.length})`)
+        }
+
+        query += ` AND (${conditions.join(' OR ')})`
+
+        console.log(query)
+
+        const result = await db.query(query, values)
 
         for(const { user_id } of result.rows){
             if (text) await sendText(user_id, text)
