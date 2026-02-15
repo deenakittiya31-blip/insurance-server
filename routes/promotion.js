31c2c1eddd1b90b5db36bde7c73ee4b891e575a9
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-02-15 20:31:30 +0700

    import function

--- routes/promotion.js
+++ routes/promotion.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { create, list, listSelect, read, update, is_active, remove } = require('../controllers/promotion')
+const { authCheck, roleCheck } = require('../middleware/authCheck')
 const router = express.Router()
 
 router.post('/create-promotion', authCheck, roleCheck(['admin']), create)
@@ -8,6 +9,6 @@ router.get('/list-promotion-select',listSelect)
 router.get('/read-promotion/:id', authCheck, roleCheck(['admin']), read)
 router.put('/update-promotion/:id', authCheck, roleCheck(['admin']), update)
 router.put('/status-promotion/:id', authCheck, roleCheck(['admin']), is_active)
-router.delete('/delete-promotion/:id', authCheck, remove)
+router.delete('/delete-promotion/:id', authCheck, roleCheck(['admin']), remove)
 
 module.exports = router
