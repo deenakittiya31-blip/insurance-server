4829698c4d79c6b4e06079c009924b53d2150a9a
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-02-16 17:57:55 +0700

    user route

--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,10 @@ const { authCheck, roleCheck } = require('../middleware/authCheck')
 const { list, read, update, is_active, remove } = require('../controllers/user')
 const router = express.Router()
 
-router.get('/list-promotion', authCheck, roleCheck(['admin']), list)
-router.get('/read-promotion/:id', authCheck, roleCheck(['admin']), read)
-router.put('/update-promotion/:id', authCheck, roleCheck(['admin']), update)
-router.put('/status-promotion/:id', authCheck, roleCheck(['admin']), is_active)
-router.delete('/delete-promotion/:id', authCheck, roleCheck(['admin']), remove)
+router.get('/list-user', authCheck, roleCheck(['admin']), list)
+router.get('/read-user/:id', authCheck, roleCheck(['admin']), read)
+router.put('/update-user/:id', authCheck, roleCheck(['admin']), update)
+router.put('/status-user/:id', authCheck, roleCheck(['admin']), is_active)
+router.delete('/delete-user/:id', authCheck, roleCheck(['admin']), remove)
 
 module.exports = router
