e33892d0c7249f9c625bd180d0959fc75fa90ef2
kittiyaporn31 <kkittiyaporn2003@gmail.com>
2026-03-09 10:10:29 +0700

    get path

--- routes/setting.js
+++ routes/setting.js
@@ -3,7 +3,7 @@ const { authCheck, roleCheck } = require('../middleware/authCheck');
 const { updateSettingSecret, listSettingSecret } = require('../controllers/setting');
 const router = express.Router();
 
-router.post('/setting', authCheck, roleCheck(['admin']), listSettingSecret  )
+router.get('/setting', authCheck, roleCheck(['admin']), listSettingSecret  )
 router.put('/setting/:id', authCheck, roleCheck(['admin']), updateSettingSecret)
 
 module.exports = router
